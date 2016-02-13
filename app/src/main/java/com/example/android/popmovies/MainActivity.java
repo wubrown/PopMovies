c096d55be3afcbbcd272751f7beb3de487cc5dc2
William Brown <wubrown@gmail.com>
2016-02-12 17:13:59 -0800

    fix: Downgrade shell app to SDK 21 from SDK 23

--- app/src/main/java/com/example/android/popmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popmovies/MainActivity.java
@@ -1,29 +1,18 @@
 package com.example.android.popmovies;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
- /*       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        }); */
-    }
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
