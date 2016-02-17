f822440ede75a44ec7064616ec8ca179a4ffd03c
William Brown <wubrown@gmail.com>
2016-02-17 14:25:43 -0800

    feat: Add GridView with test ImageAdapter

--- app/src/main/java/com/example/android/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popmovies/MainActivityFragment.java
@@ -1,22 +1,100 @@
 package com.example.android.popmovies;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
- * A placeholder fragment containing a simple view.
+ * A Fragment containing a GridView of movie options.
  */
 public class MainActivityFragment extends Fragment {
-
+ //   private ArrayAdapter<String> mMovieAdapter;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+ /*       String[] data = {
+                "sample_0.jpg",
+                "sample_1.jpg",
+                "sample_2.jpg",
+                "sample_3.jpg",
+                "sample_4.jpg",
+                "sample_5.jpg",
+                "sample_6.jpg",
+                "sample_7.jpg"
+        };
+        List<String> movieOptions = new ArrayList<String>(Arrays.asList(data));
+
+        mMovieAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.grid_item_movie,
+                R.id.grid_item_movie_imageview,
+                movieOptions); */
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movie);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+        return rootView;
     }
+/**
+ * ImageAdapter from Grid View API on android.com
+ */
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(170, 170));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}
 }
