9dd62e19e9a93a715973dcf475c881f5c79ca8ef
William Brown <wubrown@gmail.com>
2016-02-29 10:37:21 -0800

    feat: Add blank settings activity
    Model after course provided Sunshine2 - Branch 3.06

--- app/src/main/java/com/example/android/popmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popmovies/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.android.popmovies;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -45,6 +47,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
